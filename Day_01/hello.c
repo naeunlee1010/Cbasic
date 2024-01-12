92bc67a66eda1c96241de10023673beb4c3f9ba5
naeunlee1010 <152273213+naeunlee1010@users.noreply.github.com>
2024-01-12 11:04:14 +0900

    Add files via upload

---
+++ Day_01/hello.c
@@ -0,0 +1,12 @@
+//slash:주석(comment)
+//창 나오게 하려면 디버그>디버그하지 말고 시작 
+/*
+여러 줄 주석처리 가능
+코딩에 적합한 폰트: consolas, lucida console
+*/
+
+#include <stdio.h> //헤더파일
+int main() {
+	printf("hello world!!\n");
+	return 0;
+}
---
+++ Day_01/hello4.c
@@ -0,0 +1,11 @@
+//콘솔창에 본인 이름, 나이, 주소를 출력
+//홍길동 
+//20세
+//서울시 서대문구
+#include <stdio.h>
+int main() {
+	printf("이나은\n");
+	printf("25세 \n");
+	printf("서울시 성북구\n");
+	return 0;
+}
